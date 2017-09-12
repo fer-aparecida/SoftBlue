047695a4589b39645a372ed020199ec06b06d715
Administrador <jcricardomedeiros@gmail.com>
2017-09-12 10:31:08 -0300

    Mudanças drásticas para seu conhecimento

--- Aula7/Formularios_SimplesForm.php
+++ Aula7/Formularios_SimplesForm.php
@@ -4,11 +4,45 @@ header('Content-type: text/html; charset=iso-8859-1');
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>		
 		<title>Formulário Simples: Form</title>
 	</head>
 	<body>
-		<form method=POST action="Formularios_SimplesTratamento.php">
-			Nome: 
+		<form method=POST action="Formularios_SimplesTratamento.php" >
+		  <table width="200px" align="center" style="margin-top: 50px;">
+		  <tr><td>
+			<table class="table table-striped">
+				<tr>
+					<td>Nome:</td>
+					<td><input type=TEXT name=nome></td>
+				</tr>
+				<tr>
+					<td>Sobrenome:</td>
+					<td><input type=TEXT name=sobrenome></td>
+				</tr>
+				<tr>
+					<td>Estado Civil:</td>
+					<td>
+						<select name=estadocivil class="form-control">
+							<option>Solteiro</option>
+							<option>Casado</option>
+							<option>Divorciado</option>
+							<option>Viúvo</option>
+						</select>
+					</td>
+				</tr>				
+				<tr>
+					<td colspan="2" align="center">
+						<button type="RESET" class="btn btn-primary">Limpar</button>
+						<button type="SUBMIT" class="btn btn-success">Enviar</button>
+					</td>
+				</tr>
+			</table></td></tr>
+			</table>
+			<!-- Nome: 
 			<input type=TEXT name=nome><br>
 			Sobrenome:
 			<input type=TEXT name=sobrenome><br>
@@ -21,7 +55,7 @@ header('Content-type: text/html; charset=iso-8859-1');
 			</select>
 			<br><br>
 			<input type=RESET value="Limpar">
-			<input type=SUBMIT value="Enviar">
+			<input type=SUBMIT value="Enviar"> -->
 		</form>
 	</body>
 </html>	
--- Aula7/Formularios_SimplesTratamento.php
+++ Aula7/Formularios_SimplesTratamento.php
@@ -1,12 +1,30 @@
+<?php
+header('Content-type: text/html; charset=iso-8859-1');
+?>
 <html>
 <head>
 	<title>Formulários Simples: Tratamento</title>
 </head>
 	<body>
 <?php
-	echo "Nome informado: " . $_POST["nome"] . "<br>";
-	echo "Sobrenome informado: " . $_POST["sobrenome"] . "<br>";
-	echo "Nome informado: " . $_POST["nome"] . "<br>";
+	if (isset($_POST["nome"])) {
+		echo "Nome informado: " . $_POST["nome"] . "<br>";
+	}
+	else {
+		echo "Nome não informado". "<br>";
+	}
+	if (isset($_POST["sobrenome"])) {
+		echo "Sobrenome informado: " . $_POST["sobrenome"] . "<br>";
+	}
+	else {
+		echo "Sobre Nome não informado";
+	}
+	if (isset($_POST["estadocivil"])) {
+		echo "Estado Civil informado: " . $_POST["estadocivil"] . "<br>";
+	}
+	else {
+		echo "Estado Civil não informado";
+	}
 ?>	
 	</body>
 </html>	
