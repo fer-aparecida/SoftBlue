6090421b891256693eb3f10cd04450591d0f7b25
Administrador <fer_aparecida@hotmail.com>
2017-09-13 13:58:37 -0300

    Formulários Avançados

---
+++ Aula7/Fourmularios_Upload.php
@@ -0,0 +1,21 @@
+<?php
+	header('Content-type: text/html; charset=iso-8859-1');
+?>
+	
+<html>
+	<head>
+		<title>Formulário Upload</title>
+	</head>
+	<body>
+<?php
+
+?>
+	<form enctype="multipart/form-data" method=POST action="Formularios_Upload.php?envio=true">
+	
+		
+	</form>
+	</body>
+</html>
+
+
+<!--<?php?>-->
